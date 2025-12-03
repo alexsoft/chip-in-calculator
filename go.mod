a80bf6cfa37dc855c95ce0ecf77ba235a7c6f4e8
Oleksii Plekhanov <1451894+alexsoft@users.noreply.github.com>
2025-12-04 00:07:49 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alexsoft/chip-in-go
+
+go 1.25.5
+
+require github.com/umputun/go-flags v1.5.1
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+
+	"github.com/umputun/go-flags"
+)
+
+const netflixMembersCount = 3
+const netflixPrice = 21.99
+
+const spotifyMembersCount = 5
+const spotifyPrice = 21.99
+
+var opts struct {
+	ExchangeRate float64 `short:"r" long:"rate" description:"current EUR -> UAH rate"`
+}
+
+func main() {
+	fmt.Printf("chip-in-go\n")
+
+	flags.Parse(&opts)
+
+	if opts.ExchangeRate <= 0 {
+		fmt.Println("Exchange rate is not provided")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Exchange rate: %v\n", opts.ExchangeRate)
+
+	calculateWithExchangeRate(opts.ExchangeRate)
+}
+
+func calculateWithExchangeRate(rate float64) {
+	fmt.Println("Netflix:", math.Round(netflixPrice/netflixMembersCount*rate), "UAH")
+	fmt.Println("Spotify:", math.Round(spotifyPrice/spotifyMembersCount*rate), "UAH")
+}
